57f148b5b6ca792e71ffb75d868a8603a7368621
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2022-07-08 01:27:29 -0400

    First commit for python calculator

---
+++ project10_calculator.py/main.py
@@ -0,0 +1,35 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+num1 = int(input("What's the first number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What's the second number?: "))
+
+
+def calculate(n1, n2, symbol):
+
+    return operations[symbol](n1, n2)
+
+
+answer = calculate(num1, num2, operation_symbol)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
