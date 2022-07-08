32382423c58ae66aac65af8dadac0dcc26029609
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2022-07-07 22:03:02 -0400

    Completed functions. App now working properly.

--- project9_secret_auction/main.py
+++ project9_secret_auction/main.py
@@ -1,46 +1,31 @@
-bids = [{"name": "Felipe", "bid": 100}, {"name": "Carlos", "bid": 200}]
-
-
 def take_bid(name, bid):
     bids.append({"name": name, "bid": bid})
-    print(bids)
-
-
-def grab_bid(bids):
-    highest = 0
-
-    for item in bids:
-        if highest > item["bid"]:
-            highest = item["bid"]
-            print(highest)
-
-
-grab_bid(bids)
 
 
 def highest_bid(bids):
-    highest = 0
+    highest_bidder = {"name": "", "bid": 0}
 
     for item in bids:
-        for key in item:
-            if bids["bid"] > bids.index(item["bid"] + 1):
-                highest = item["bid"]
-            print(f'The highest bidder is {item["name"]} with a ${item["bid"]} bid.')
+        if highest_bidder["bid"] < item["bid"]:
+            highest_bidder = item
+
+    print(
+        f'The highest bidder is {highest_bidder["name"]} with a ${highest_bidder["bid"]} bid.'
+    )
 
 
+bids = []
 bid_active = True
 
-# while bid_active:
-#     name = input("What is your name?: ")
-#     bid = int(input("What is your bid?: "))
+while bid_active:
+    name = input("What is your name?: ")
+    bid = int(input("What is your bid?: "))
 
-#     highest_bidder = ""
+    take_bid(name, bid)
 
-#     take_bid(name, bid)
+    more_bidders = input("Are more people bidding?: ")
+    more_bidders.lower()
 
-#     more_bidders = input("Are more people bidding?: ")
-#     more_bidders.lower()
-
-#     if more_bidders == "no":
-#         print(bids[2])
-#         bid_active = False
+    if more_bidders == "no":
+        highest_bid(bids)
+        bid_active = False
