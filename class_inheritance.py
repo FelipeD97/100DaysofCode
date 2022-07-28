3b81f093cd3b6c41b42696b8236b98fc39107e39
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2022-07-28 16:41:37 -0400

    First commit, class inheritance practice.

---
+++ class_inheritance.py
@@ -0,0 +1,29 @@
+from unicodedata import name
+
+
+class Animal:
+    def __init__(self):
+        self.number_eyes = 2
+
+    def breathe(self):
+        print("Inhale... Exhale...")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breathe(self):
+        super().breathe()
+        print("Doing this underwater")
+
+    def swim(self):
+        print("One stroke... two strokes...")
+
+
+nemo = Fish()
+rodger = Animal()
+
+rodger.breathe()
+nemo.breathe()
+nemo.swim()
