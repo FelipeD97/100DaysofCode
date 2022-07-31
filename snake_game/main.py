6aa327986ffe31fb4b7fcaf05c37f7811a342543
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2022-07-31 18:58:12 -0400

    Added scoreboard class and completed food class. Snake can eat food and keep score.

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("Welcome to the Snake Game!")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(key="Left", fun=snake.turn_left)
@@ -22,5 +26,18 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    # Detect collision with food
+
+    if snake.head.distance(food) < 15:
+        # If snake head is within 15 pixels of the food then they've collided
+        print("nom... nom... nom...")
+        food.update()
+        scoreboard.increase_score()
+
+    # if snake.head.distance(screen) < 20:
+    #     print("You crashed!\nGame Over")
+    #     game_is_on = False
+
 
 screen.exitonclick()
