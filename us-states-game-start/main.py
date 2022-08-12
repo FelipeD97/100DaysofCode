8efd83ee3cb239b164556a3701b55b4e99bd6c99
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2022-08-11 21:49:38 -0400

    Now game saves list of unguessed states.

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -7,29 +7,36 @@ image = "us-states-game-start/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-state_name = turtle.Turtle()
-state_name.hideturtle()
-
 data = pandas.read_csv("us-states-game-start/50_states.csv")
 all_states = data.state.to_list()
 
-answers = []
+guessed_states = []
 
-while len(answers) < 50:
+player_name = screen.textinput(title="Guess the State", prompt="What is your name?")
+
+while len(guessed_states) < 50:
 
     answer_state = screen.textinput(
-        title=f"Guess the State ({len(answers)}/50)",
+        title=f"Correct States ({len(guessed_states)}/50)",
         prompt="What's another state name?",
     )
 
-    capital_state = answer_state.title()
+    capitalized_state = answer_state.title()
 
-    for state in all_states:
-
-        if capital_state == state:
-            correct_state = data[data.state == capital_state]
-            state_name.penup()
-            state_name.goto(int(correct_state.x), int(correct_state.y))
-            state_name.write(capital_state)
-            if capital_state not in answers:
-                answers.append(capital_state)
+    if capitalized_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+                new_data = pandas.DataFrame(missing_states)
+                new_data.to_csv(f"us-states-game-start/{player_name}StatesToLearn.csv")
+        break
+    if capitalized_state in all_states:
+        correct_state = data[data.state == capitalized_state]
+        state_name = turtle.Turtle()
+        state_name.hideturtle()
+        state_name.penup()
+        state_name.goto(int(correct_state.x), int(correct_state.y))
+        state_name.write(capitalized_state)
+        if capitalized_state not in guessed_states:
+            guessed_states.append(capitalized_state)
