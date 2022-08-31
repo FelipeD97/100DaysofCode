65582d3ba30bb3d0c758c2d6e471a0995efc3ab3
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2022-08-31 00:38:55 -0400

    Fixed syntax error

--- quizzler-app-start/data.py
+++ quizzler-app-start/data.py
@@ -1,6 +1,6 @@
 import requests
 
-parameters = {"amount": 10, "type": "boolean"}
+parameters = {"amount": 10, "type": "boolean", "category": 14}
 
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
