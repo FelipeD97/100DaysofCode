ea2ab02a18b56c1c3cca6f640a20b7a57ae7878f
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2022-12-20 00:17:41 -0500

    Updated html to attribute bootstrap styling and added routing for about me and contact me pages.

--- Day 59 - Blog Capstone/startbootstrap-clean-blog-gh-pages/main.py
+++ Day 59 - Blog Capstone/startbootstrap-clean-blog-gh-pages/main.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request
+from posts import Post
 import requests
 
 app = Flask(__name__)
 
+posts = requests.get("https://api.npoint.io/0912c5d990db3873f519").json()
+post_objects = []
+
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+
 @app.route("/")
 def homepage():
     return render_template("index.html")
-    
+
+@app.route("/about")
+def about_me():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact_me():
+    return render_template("contact.html")
 if __name__ == '__main__':
     app.run(debug=True)
